cacb7cfdb0c79a4de957d0d7a1d3272b7bc09698
ashle <ashle@DESKTOP-VVPHGA7>
2025-02-19 19:31:10 -0400

    Add project files.

---
+++ CapUsuario/Program.cs
@@ -0,0 +1,22 @@
+﻿using CapaNegocio;
+namespace CapUsuario
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("elija el animal que desea crear ");
+
+            Console.WriteLine("[1] Cuadrupedo [2] ave [3]pez ");
+
+            int opcion =Convert.ToInt32( Console.ReadLine()!); 
+
+            Animal animal = Factory.GetAnimal(opcion);
+
+            animal.TipoAnimal();
+            Console.ReadKey();
+            Console.Clear();
+
+        }
+    }
+}
---
+++ CapaNegocio/Factory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocio
+{
+    public class Factory
+    {
+
+        public static Animal GetAnimal(int opcion)
+        {
+            switch (opcion)
+            {
+                case 1:
+                    return new Cuadrupedo();
+
+                case 2:
+                    return new Ave();
+
+                case 3:
+                    return new Pez();
+
+                default:
+
+                    return null;
+            }
+        }
+    }
+}
